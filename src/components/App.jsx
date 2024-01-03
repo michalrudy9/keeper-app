df030628ca4d38f33176e64f6f15706e948891c2
michalrudy9 <michallrudy9@gmail.com>
2024-01-03 18:35:42 +0100

    Added adding new note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(note) {
+    setNotes((prevNotes) => [...prevNotes, note]);
+  }
+
   return (
     <div>
       <Header />
-      <Note />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem) => (
+        <Note title={noteItem.title} content={noteItem.content} />
+      ))}
       <Footer />
     </div>
   );
